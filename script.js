b801fbb1c132f66160289bcaf77fe35c47fdae03
lacerda <edulacerdaaa@gmail.com>
2025-02-08 22:09:36 -0300

    feat:catching the submit event and handling it

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 // select the form inputs
-
+const form = document.querySelector('form')
+const amount = document.getElementById('amount');
+const expense = document.getElementById('expense')
+const category = document.getElementById('category')
 
 //amount input: just-numbers validation 
-const amount = document.getElementById('amount');
 amount.oninput = () => {
-    let value = amount.value.replace(/\D/g, "");
-    value = Number(value) / 100 
-    
-    amount.value = formatCurrencyBRL(value)
+    let value = amount.value.replace(/\D/g, ""); //uses regex to remove all the non numeric characters
+    value = Number(value) / 100 // transform the first inputs in cents
+    amount.value = formatCurrencyBRL(value) // updates the input field value with the formated value
 }
 
-
+//function to format number to the brazilian currency
 function formatCurrencyBRL(value) {
     value = value.toLocaleString("pt-BR", {
         style: "currency",
         currency: "BRL"
     })
-
     return value
+}
+
+//catches the form submit event
+form.onsubmit = (event) => {
+    event.preventDefault() // prevents default form event behavior
 }
